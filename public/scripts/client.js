eeb38ab34175a163e237c44df88328b72ff0ed0a
Thushara <thusharan@hotmail.com>
2022-04-28 23:03:35 -0400

    added data and started dynamic tweet fcns

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,47 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+//Client-side JS logic goes here.  jQuery is already loaded
+//Reminder: Use (and do all your DOM work in) jQuery's document ready function
 
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  },
+  {
+    "user": {
+      "name": "Donald Glover",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@childishMoschino"
+    },
+    "content": {
+      "text": "I'm on here cause I heard Elon Musk just bought up twitter :("
+    },
+    "created_at": 1461116232247
+  },
+]
+
+const renderTweets = function(tweets) {
+
+}
+
+
+
+renderTweets(data)
