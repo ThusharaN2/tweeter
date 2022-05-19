331f451ca9587f56557da2c2fb0fc684be9e139d
Thushara <thusharan@hotmail.com>
2022-05-19 15:01:05 -0400

    fixed the icons to go to the right while timeago is on the left

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,6 @@
 //Client-side JS logic goes here.  jQuery is already loaded
 //Reminder: Use (and do all your DOM work in) jQuery's document ready function
- 
+
 $(document).ready(function () {
   const escape = function (str) {
     let div = document.createElement("div");
@@ -9,8 +9,8 @@ $(document).ready(function () {
   };
 
   //creating new tweet 
-const createTweetElement = function(tweetBody) { 
-  let $tweet = (`
+  const createTweetElement = function (tweetBody) {
+    let $tweet = (`
   <article class="tweet">
             <header>
               <div class = "user">
@@ -27,6 +27,7 @@ const createTweetElement = function(tweetBody) {
             <footer id='tweet-foot'>
               <div class='foot-content'>
               <p>${timeago.format(tweetBody.created_at)}</p>
+              </div>
               <div class='icons'>
                 <i class="fa-solid fa-flag"></i>
                 <i class="fa-solid fa-repeat"></i>
@@ -36,42 +37,41 @@ const createTweetElement = function(tweetBody) {
             </footer>
           </article>
           `);
-          return $tweet;
+    return $tweet;
   }
 
-//loops through tweets & calls createTweetElement for every tweet 
-const renderTweets = function(tweetsArray) { 
-  // $(".tweet-box").empty()
-  for (const tweet of tweetsArray) {
-  $('.tweet-box').prepend(createTweetElement(tweet));
- }
-}
+  //loops through tweets & calls createTweetElement for every tweet 
+  const renderTweets = function (tweetsArray) {
+    for (const tweet of tweetsArray) {
+      $('.tweet-box').prepend(createTweetElement(tweet));
+    }
+  }
 
-//sends message if over character limit or if no characters present when posting 
+  //sends message if over character limit or if no characters present when posting 
   //if anything, default shouldn't happen
-const $tweetForm = $(".tweetform"); 
+  const $tweetForm = $(".tweetform");
   $tweetForm.submit(function (event) {
     const tweettext = $("#tweet-text").val();
-console.log(tweettext)
-event.preventDefault()
+    console.log(tweettext)
+    event.preventDefault()
     if (tweettext.length === 0) {
       $("🤡 OOPS, looks like you didn't tweet anything 🤡...").slideDown();
     } else if (tweettext.length > 140) {
       $("🥴 WOAH, hold up, please respect the max character count🥴!!!").slideUp();
     }
-    $.ajax("http://localhost:8080/tweets/", {
+    $.ajax("/tweets/", {
       method: "POST",
       data: $(this).serialize(),
-    }).then(() => {$(this).serialize()});      
+    }).then(() => { $(this).serialize() });
     loadtweets();
-});
-
-//load Tweets from db 
-const loadtweets = function () {
-  $.ajax("/tweets", { method: "GET" }).then((arr) => {
-    $(".tweet-box").empty();
-    renderTweets(arr);
   });
-};
-loadtweets();
+
+  //load Tweets from db 
+  const loadtweets = function () {
+    $.ajax("/tweets", { method: "GET" }).then((arr) => {
+      $(".tweet-box").empty();
+      renderTweets(arr);
+    });
+  };
+  loadtweets();
 });
